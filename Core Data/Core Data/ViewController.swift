43da778c5df8f4490b21ed7efed1ad698612fad7
Bernard Huff <bernard.huff008@gmail.com>
2017-07-03 17:53:10 -0500

    Test 1

---
+++ Core Data/Core Data/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Core Data
+//
+//  Created by Bernard Huff on 7/3/17.
+//  Copyright © 2017 Flyhightech.LLC. All rights reserved.
+//
+
+import UIKit
+
+// This is just a test to see if the github initialized correctly.
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
