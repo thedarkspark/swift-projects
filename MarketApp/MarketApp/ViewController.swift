52c3666436e2700838b2f53a4571d4d303ab1fa2
Bernard Huff <bernard.huff008@gmail.com>
2017-07-05 02:26:45 -0500

    Starting work on connecting the logic

--- MarketApp/MarketApp/ViewController.swift
+++ MarketApp/MarketApp/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func enterBtn(_ sender: Any) {
+        
     }
-
 
 }
 
