2559ce67ba6ff92d767edfcd8a55f3edeb8bcc1c
Bernard Huff <bernard.huff008@gmail.com>
2017-07-08 02:09:26 -0500

    Starting the path

---
+++ Miracle Pills/Miracle Pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Miracle Pills
+//
+//  Created by Bernard Huff on 7/8/17.
+//  Copyright © 2017 Bernard Huff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
