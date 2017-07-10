5c08134654f9022461c6d5a4a1a14512b29eab7a
Bernard Huff <bernard.huff008@gmail.com>
2017-07-10 01:51:33 -0500

    Now were off to the races!

--- Miracle Pills/Miracle Pills/ViewController.swift
+++ Miracle Pills/Miracle Pills/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var stateBtnText: UIButton!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        statePicker.dataSource = self
+        
+        statePicker.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 0
+        
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
