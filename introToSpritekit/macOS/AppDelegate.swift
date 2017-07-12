796b008ce98fb069574bb23501bcd56c90a0aa04
Bernard Huff <bernard.huff008@gmail.com>
2017-07-12 03:37:31 -0500

    Intro into sprite kit

---
+++ introToSpritekit/macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  introToSpritekit
+//
+//  Created by Bernard Huff on 7/12/17.
+//  Copyright © 2017 Bernard Huff. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
