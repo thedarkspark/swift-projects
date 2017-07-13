bb8b3a5c3b2fdb30f12e2291c739810f08a0c0b4
Bernard Huff <bernard.huff008@gmail.com>
2017-07-12 19:58:44 -0500

    Added image assets to the market app.

---
+++ marketplaceapp/marketplaceapp/addItemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  addItemViewController.swift
+//  marketplaceapp
+//
+//  Created by Bernard Huff on 7/12/17.
+//  Copyright © 2017 Bernard Huff. All rights reserved.
+//
+
+import UIKit
+
+class addItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
