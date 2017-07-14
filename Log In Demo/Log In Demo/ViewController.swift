c5d3319214138a2978466f265ee960098c164970
Bernard Huff <bernard.huff008@gmail.com>
2017-07-14 10:11:03 -0500

    Added some elements to a login screen.

---
+++ Log In Demo/Log In Demo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Log In Demo
+//
+//  Created by Bernard Huff on 7/14/17.
+//  Copyright © 2017 Bernard Huff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
