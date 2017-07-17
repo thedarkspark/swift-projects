3256fa6b6b7ec3704f05e020e96d1feebb66a878
Bernard Huff <bernard.huff008@gmail.com>
2017-07-17 02:17:57 -0500

    Added a couple of sprites to the scene and made them a different color. Now to get them to the view.

--- introIntoSpriteKit/introIntoSpriteKit/GameScene.swift
+++ introIntoSpriteKit/introIntoSpriteKit/GameScene.swift
@@ -12,13 +12,34 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var sprite1: SKSpriteNode?
+    
+    var sprite2: SKSpriteNode?
+    
+    var label: SKLabelNode?
+    
     override func didMove(to view: SKView) {
+        
+        sprite1 = self.childNode(withName: "spriteNode1") as? SKSpriteNode
+        
+        sprite2 = self.childNode(withName: "spriteNode2") as? SKSpriteNode
+        
+        label = self.childNode(withName: "labelNode") as? SKLabelNode
+        
+        label?.text = "Nodes Tutorial"
+        
+        sprite1?.position = CGPoint(x: 0, y: -500)
+        
+        sprite2?.color = UIColor.blue
        
     }
     
 // Last thing I did was delete all the touch is functions.
     
     override func update(_ currentTime: TimeInterval) {
+        
         // Called before each frame is rendered
+        
     }
+    
 }
