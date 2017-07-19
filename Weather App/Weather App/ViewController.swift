8903e11f66b85681700b2b3091e14514ff8a0914
Bernard Huff <bernard.huff008@gmail.com>
2017-07-19 17:27:14 -0500

    Sweep

--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -14,16 +14,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        _ = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=bbcc2a4f563a9c647d9a642cbf94b17e")
+        _ = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=bbcc2a4f563a9c647d9a642cbf94b17e")!
         
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in if error != nil {
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
             
                 print(error)
             
         } else {
             
             if let urlContent = data {
-            
+                
+                do {
+                
+                let jsonResult = JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
+                
+                print(jsonResult)
+                    
                     
                 
                 }
@@ -38,7 +46,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
