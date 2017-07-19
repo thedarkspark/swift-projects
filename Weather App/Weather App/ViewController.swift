386bdc420a165131d9081c0e74a9600ce70ac737
Bernard Huff <bernard.huff008@gmail.com>
2017-07-19 02:45:06 -0500

    json data

---
+++ Weather App/Weather App/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Bernard Huff on 7/18/17.
+//  Copyright © 2017 Bernard Huff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        _ = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=bbcc2a4f563a9c647d9a642cbf94b17e")
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in if error != nil {
+            
+                print(error)
+            
+        } else {
+            
+            if let urlContent = data {
+            
+                    
+                
+                }
+            
+            }
+        
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
