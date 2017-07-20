1c099f15144409efd703bf1206696a6c83e1fef8
Bernard Huff <bernard.huff008@gmail.com>
2017-07-20 10:51:33 -0500

    Added exceptions to the p-list that allow other website date to be injected using JSON

--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -14,13 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        _ = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=bbcc2a4f563a9c647d9a642cbf94b17e")!
+        let url = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=bbcc2a4f563a9c647d9a642cbf94b17e")!
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             
             if error != nil {
             
-                print(error)
+                print("Error")
             
         } else {
             
@@ -28,24 +28,31 @@ class ViewController: UIViewController {
                 
                 do {
                 
-                let jsonResult = JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
+                let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
                 
                 print(jsonResult)
                     
                     
                 
+                } catch {
+                
+                    print("JSON data failed")
+                    
                 }
             
             }
         
         }
-        
+            
     }
-
-    override func didReceiveMemoryWarning() {
+     
+    task.resume()
+    
+    
+    func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 }
-
+}
