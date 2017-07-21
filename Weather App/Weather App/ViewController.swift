7ec7a65d2535cf7735ab37137f95fa5e1a862fa1
Bernard Huff <bernard.huff008@gmail.com>
2017-07-21 09:35:55 -0500

    Force unwrapped the textField.text so that the user can update the textField with their information.

--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -16,57 +16,59 @@ class ViewController: UIViewController {
     
     @IBAction func submitButton(_ sender: Any) {
         
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q= " + cityTextField.text! + ",uk&appid=bbcc2a4f563a9c647d9a642cbf94b17e")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
+                
+                print("Error")
+                
+            } else {
+                
+                if let urlContent = data {
+                    
+                    do {
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
+                        
+                        print(jsonResult)
+                        
+                        print(jsonResult["name"])
+                        
+                        if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)? ["description"] as? String {
+                            
+                            DispatchQueue.main.sync(execute: self.resultLabel.text = description)
+                            
+                        }
+                        
+                    } catch {
+                        
+                        print("JSON data failed")
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        task.resume()
+
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=bbcc2a4f563a9c647d9a642cbf94b17e")!
-        
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            
-            if error != nil {
-            
-                print("Error")
-            
-        } else {
-            
-            if let urlContent = data {
-                
-                do {
-                
-                let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
-                
-                print(jsonResult)
-                    
-                   print(jsonResult["name"])
-                    
-                    if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)? ["description"] as? String {
-                        
-                        print(description)
-                    
-                    }
-                
-                } catch {
-                
-                    print("JSON data failed")
-                    
-                }
-            
-            }
-        
-        }
-            
     }
-     
-    task.resume()
     
-    
-    func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 }
-}
+
