fa5b78bcf3c5878a985acb0050768faea1d37bb9
Bernard Huff <bernard.huff008@gmail.com>
2017-07-22 09:53:11 -0500

    Added some functions to allow for the change between the player and their image.

--- Tic Tac Toe/Tic Tac Toe/TTTImageView.swift
+++ Tic Tac Toe/Tic Tac Toe/TTTImageView.swift
@@ -16,7 +16,22 @@ class TTTImageView: UIImageView {
     
     func setPlayer (_player:String) {
     
-        self.player = _player 
+        self.player = _player
+        
+        if activated == false {
+        
+            if _player == "x" {
+            
+                self.image = UIImage(named: "x")
+            
+            } else {
+            
+                self.image = UIImage(named: "o")
+            }
+            
+            activated = true 
+            
+        }
     
     }
 
