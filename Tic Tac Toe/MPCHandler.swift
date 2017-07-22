00b4ad73e9bc36ef34cb566106c2b64569572d6d
Bernard Huff <bernard.huff008@gmail.com>
2017-07-22 14:00:14 -0500

    Sweep

---
+++ Tic Tac Toe/MPCHandler.swift
@@ -0,0 +1,16 @@
+//
+//  MPCHandler.swift
+//  Tic Tac Toe
+//
+//  Created by Bernard Huff on 7/22/17.
+//  Copyright © 2017 Bernard Huff. All rights reserved.
+//
+//  On youtube the marker is about 16:30 when I left off.
+
+import UIKit
+
+import MultipeerConnectivity
+
+class MPCHandler: NSObject {
+
+}
