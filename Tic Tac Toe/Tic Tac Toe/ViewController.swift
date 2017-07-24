cfb86d9763375c34f8614f319fb5455a8a7903d2
Bernard Huff <bernard.huff008@gmail.com>
2017-07-23 21:56:27 -0500

    Connecting the dots. Crossing the T's and dotting the i's

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -4,15 +4,32 @@
 //
 //  Created by Bernard Huff on 7/22/17.
 //  Copyright © 2017 Bernard Huff. All rights reserved.
-//
+//  
+//  I left off at 34:00 on the video
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var fields: [TTTImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setupGameLogic() {
+    
+        for _ in 0 ... fields.count - 1 {
+        
+            let gestureRecognizer = UITapGestureRecognizer(target: self, action: Selector(("fieldTapped:")))
+            
+            gestureRecognizer.numberOfTapsRequired = 1
+            
+            fields[index].addGestureRecognizer(gestureRecognizer)
+            
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
